f043ed5de152e6f1c351d2555f2918c3a720ad6e
Anton Shvein aka T0ha <t0hashvein@gmail.com>
2025-08-29 08:14:32 +0300

    Refactored to useForm and added validation

--- src/schedule-post.tsx
+++ src/schedule-post.tsx
@@ -1,23 +1,13 @@
-import { Form, ActionPanel, Action, LaunchProps, showToast, getPreferenceValues } from "@raycast/api";
-import { useCachedState } from "@raycast/utils";
+import { Form, ActionPanel, Action, LaunchProps, ToastStyle, showToast, getPreferenceValues } from "@raycast/api";
+import { useCachedState, useForm, FormValidation } from "@raycast/utils";
 import { useState, useEffect } from "react";
 
 import { Post, Platform } from "./types";
 import { fetchPlatforms, schedulePost } from "./api";
+import { countReset, trace } from "console";
 
 export default function Command(props: LaunchProps<{ draftValues: Post }>) {
-  async function handleSubmit(post: Post) {
-    try {
-      await schedulePost(api_key, post);
-      showToast({ title: "Submitted form", message: "Your post scheduled successfully" });
-      setContent("");
-      setDue(null);
-      setPlatforms([]);
-    } catch (error) {
-      console.error("Error scheduling post:", error);
-      showToast({ title: "Error", message: "Failed to schedule post" });
-    }
-  }
+  const { draftValues: draftPost } = props;
 
   const api_key = getPreferenceValues<ExtensionPreferences>().api_key;
 
@@ -30,10 +20,40 @@ export default function Command(props: LaunchProps<{ draftValues: Post }>) {
     },
   ]);
 
-  const { draftValues: draftPost } = props;
-  const [content, setContent] = useState<string>(draftPost?.content || "");
-  const [due, setDue] = useState<Date | null>(draftPost?.scheduledTime || null);
-  const [platforms, setPlatforms] = useState<string[]>(draftPost?.platforms || []);
+  const { handleSubmit, itemProps, values } = useForm<Post>({
+    initialValues: draftPost || {content: ""},
+    async onSubmit(post: Post) {
+      try {
+        await schedulePost(api_key, post);
+        showToast({ title: "Submitted form", message: "Your post scheduled successfully" });
+        return true;
+      } catch (error) {
+        console.error("Error scheduling post:", error);
+        showToast({ title: "Error", message: "Failed to schedule post" });
+      }
+    },
+  validation: {
+      platforms: FormValidation.Required,
+      content: (value) => {
+        if (value.length === 0) {
+          return "Content cannot be empty";
+        }
+        if (value.length > 280 && values.platforms && values.platforms.findIndex((v) => v.startsWith("twitter")) != -1) {
+          showToast({ title: "Too long for X", message: "280 characters limit", style: ToastStyle.Failure });
+        }
+        if (value.length > 300 && values.platforms && values.platforms.findIndex((v) => v.startsWith("bluesky")) != -1) {
+          showToast({ title: "Too long for Bluesky", message: "300 characters limit", style: ToastStyle.Failure });
+          return "Too long for Bluesky";
+        }
+        if (value.length > 500 && values.platforms && values.platforms.findIndex((v) => v.startsWith("threads")) != -1) {
+          showToast({ title: "Too long for Threads", message: "500 characters limit", style: ToastStyle.Failure });
+          return "Too long for Threads";
+        }
+        return undefined;
+      }
+    },
+  });
+
 
   useEffect(() => {
     const getPlatforms = async () => {
@@ -49,14 +69,10 @@ export default function Command(props: LaunchProps<{ draftValues: Post }>) {
     getPlatforms();
   }, [api_key]);
 
-  const updateContent = (content: string) => {
-    setContent(content);
-  };
-
   return (
     <Form
       enableDrafts
-      navigationTitle={`Total characters: ${content.length}`}
+      navigationTitle={`Total characters: ${values.content.length}`}
       actions={
         <ActionPanel>
           <Action.SubmitForm onSubmit={handleSubmit} title="Schedule Post" />
@@ -69,10 +85,9 @@ export default function Command(props: LaunchProps<{ draftValues: Post }>) {
         id="scheduledTime"
         title="Schedule"
         type={Form.DatePicker.Type.DateTime}
-        value={due}
-        onChange={setDue}
+        {...itemProps.scheduledTime}
       />
-      <Form.TagPicker id="platforms" title="Publish to" value={platforms} onChange={setPlatforms}>
+      <Form.TagPicker id="platforms" title="Publish to" {...itemProps.platforms}>
         {allPlatforms.map((platform) => (
           <Form.TagPicker.Item
             key={platform.platformId}
@@ -83,7 +98,7 @@ export default function Command(props: LaunchProps<{ draftValues: Post }>) {
         ))}
       </Form.TagPicker>
       <Form.Separator />
-      <Form.TextArea id="content" title="Post" value={content} onChange={updateContent} />
+      <Form.TextArea id="content" title="Post" {...itemProps.content} />
     </Form>
   );
 }
